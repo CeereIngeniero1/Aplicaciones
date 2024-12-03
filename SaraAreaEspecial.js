ac87a1ee9916ab10dcac67a7935bac75fc38308c
Yeison Giraldo Ceere <yeisondeveloper@gmail.com>
2024-12-03 09:20:03 -0500

    snue

--- SaraAreaEspecial.js
+++ SaraAreaEspecial.js
@@ -123,10 +123,151 @@ async function AreaEspecial(browser) {
     const selectArea = await page.$('select[name="areaOfConcessionSlct"]');
     await selectArea.type('Otro tipo de terreno');
 
+    await page.waitForTimeout(1500);
+
+    clearTimeout(Segundopaso);
+    let Tercerpaso = setTimeout(() => {
+        console.log("ENTRO EN EL Segundopaso")
+        page.close();
+        AreaEspecial(browser);
+        clearTimeout(Segundopaso);
+    }, 20000);
+    console.log("LLegue hasta aca");
+
+
+    await page.type('#selectedApplicantInputId', '96458');
+
     await page.waitForTimeout(3000);
 
-    //clearTimeout(Segundopaso);
-    console.log("LLegue hasta aca");
+    await page.keyboard.press("Enter");
+
+    await page.waitForTimeout(550);
+
+    const addProfesional = await page.$x('//span[contains(.,"Agregar")]');
+    await addProfesional[0].click();
+
+    await page.waitForTimeout(3000);
+
+    let ArchivoAmbiental = `C:\\Aplicaciones\\Documentos\\Collective\\CertificadoAmbiental\\DocumentoRequerido.pdf`;
+
+    await page.waitForSelector(`#p_PsraRsraMandatoryDocumentToAttachId0`);
+    const RutaDelArchivoo = ArchivoAmbiental;
+    for (let i = 0; i < 5; i++) {
+        let id = `#p_PsraRsraMandatoryDocumentToAttachId${i}`
+        let ElementoControladorDeCargaaa = await page.$(id);
+        await ElementoControladorDeCargaaa.uploadFile(RutaDelArchivoo);
+
+    }
+
+
+    await page.waitForTimeout(2000);
+    await page.type('#selectedCellInputMethodSlctId', '');
+    console.log("prueba");
+
+    const selectporCeldas = await page.$('select[id="selectedCellInputMethodSlctId"]');
+    await selectporCeldas.type('U');
+
+
+
+    const continPin2 = await page.$x('//span[contains(.,"Continuar")]');
+
+    clearTimeout(Tercerpaso);
+    var band = 0;
+    while (true) {
+
+        let ciclo = setTimeout(() => {
+            console.log("ENTRO EN EL Tercerpaso")
+            page.close();
+            AreaEspecial(browser);
+            return "error";
+        }, 15000);
+        await page.waitForTimeout(1000);
+
+
+
+        function MonitorearAreas(IdArea, Area) {
+            page.evaluate(({ Area }) => {
+                document.querySelector('[id="cellIdsInptId"]').value = Area.join('');
+                angular.element(document.getElementById('cellIdsInptId')).triggerHandler('change');
+            }, { Area });
+
+            DetallesCompletos = {
+                IdArea: IdArea,
+
+                Area: Area,
+
+            }
+
+            return DetallesCompletos;
+        }
+
+        if (band == 1) {
+            // Establecer el valor directamente en el input
+            await page.evaluate(() => {
+                const eastingInput = document.getElementById('0applicantCoordinateEastingTxtId');
+                eastingInput.value = '-73,64974'; // Usar el valor con punto decimal
+                eastingInput.dispatchEvent(new Event('input', { bubbles: true })); // Disparar eventos necesarios
+
+                const northingInput = document.getElementById('0applicantCoordinateNorthingTxtId');
+                northingInput.value = '7,22473';
+                northingInput.dispatchEvent(new Event('input', { bubbles: true }));
+            });
+
+            // Repetir para los otros inputs
+            await page.evaluate(() => {
+                const eastingInput2 = document.getElementById('1applicantCoordinateEastingTxtId');
+                eastingInput2.value = '-73,64974';
+                eastingInput2.dispatchEvent(new Event('input', { bubbles: true }));
+
+                const northingInput2 = document.getElementById('1applicantCoordinateNorthingTxtId');
+                northingInput2.value = '7,22473';
+                northingInput2.dispatchEvent(new Event('input', { bubbles: true }));
+            });
+            MonitorearAreas("AreaDePrueba", ["18N03E14P01A"]);
+
+        }
+
+
+        await page.waitForTimeout(1000);
+
+        await continPin2[1].click();
+
+        await page.waitForTimeout(3000);
+
+        const Todoslosparametros = await page.$$eval("span", links =>
+            links.map(link => link.textContent)
+        );
+        let cont = 1;
+        for (let i = 0; i < Todoslosparametros.length; i++) {
+            const elemento = Todoslosparametros[i];
+            console.log(elemento);
+            if (elemento == "Vea los errores a continuación (dentro de las pestañas):") {
+                cont = 0;
+            }
+
+        }
+
+
+        clearTimeout(ciclo);
+        await page.waitForTimeout(250000);
+        if (cont == "0") {
+            console.log("Limpio El campo del area");
+            page.evaluate(() => {
+                document.querySelector('[id="cellIdsTxtId"]').value = "";
+            });
+            band++;
+            //Este es la cantidad de areas mas 1 
+            if (band == 2) {
+                band = 1;
+            }
+
+        } else {
+            break;
+        }
+
+    }
+    console.log("Sali !!!!!!!!!!!!!!!!!!");
+    await page.waitForTimeout(250000);
 
 }
 
