3b03750e5578863c44abe2a1282d064bb3dc3555
CeereIngeniero1 <soporte2ceere@gmail.com>
2025-03-20 14:08:56 -0500

    oscar oscar jijiji

--- Colle_Are_OscarCAG_141.js
+++ Colle_Are_OscarCAG_141.js
@@ -3,7 +3,7 @@ const { keyboard, mouse, Key, clipboard } = require('@nut-tree-fork/nut-js');
 const colors = require('colors');
 const user = '96232';
 const pass = 'Campeon0624*';
-var Agente = 1;
+var Agente = 0;
 
 Pagina();
 async function Pagina() {
@@ -77,7 +77,7 @@ async function AreaEspecial(browser) {
 
 
 
-            await page.type('#submitterPersonOrganizationNameId', '96458');
+            await page.type('#submitterPersonOrganizationNameId', '96232');
 
             await page.waitForTimeout(3000);
 
@@ -154,7 +154,7 @@ async function AreaEspecial(browser) {
     console.log("LLegue hasta aca");
 
 
-    await page.type('#selectedApplicantInputId', '96458');
+    await page.type('#selectedApplicantInputId', '96232');
 
     await page.waitForTimeout(3000);
 
