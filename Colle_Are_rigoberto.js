a5ac0d665e98e2513b97ced51c51225ffad0a4a7
CeereIngeniero1 <soporte2ceere@gmail.com>
2025-03-27 09:28:56 -0500

    area importante nueva

--- Colle_Are_rigoberto.js
+++ Colle_Are_rigoberto.js
@@ -246,25 +246,25 @@ async function AreaEspecial(browser) {
             // Establecer el valor directamente en el input
             await page.evaluate(() => {
                 const eastingInput = document.getElementById('0applicantCoordinateEastingTxtId');
-                eastingInput.value = '-75,53174'; // Usar el valor con punto decimal
+                eastingInput.value = '-75,5618'; // Usar el valor con punto decimal
                 eastingInput.dispatchEvent(new Event('input', { bubbles: true })); // Disparar eventos necesarios
 
                 const northingInput = document.getElementById('0applicantCoordinateNorthingTxtId');
-                northingInput.value = '5,60655';
+                northingInput.value = '5,60641';
                 northingInput.dispatchEvent(new Event('input', { bubbles: true }));
             });
 
             // Repetir para los otros inputs
             await page.evaluate(() => {
                 const eastingInput2 = document.getElementById('1applicantCoordinateEastingTxtId');
-                eastingInput2.value = '-75,53174';
+                eastingInput2.value = '-75,5618';
                 eastingInput2.dispatchEvent(new Event('input', { bubbles: true }));
 
                 const northingInput2 = document.getElementById('1applicantCoordinateNorthingTxtId');
-                northingInput2.value = '5,60655';
+                northingInput2.value = '5,60641';
                 northingInput2.dispatchEvent(new Event('input', { bubbles: true }));
             });
-            MonitorearAreas("AreaDePrueba", ["18N05A20P19T"]);
+            MonitorearAreas("AreaDePrueba", ["18N05A20N18T"]);
         }
 
         await page.waitForTimeout(500);
