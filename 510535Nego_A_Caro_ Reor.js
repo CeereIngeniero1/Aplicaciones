92daac03ec1b260e9f06b696c77cdbc594596ec7
CeereIngeniero1 <soporte2ceere@gmail.com>
2025-06-19 12:53:31 -0500

    list todo

--- 510535Nego_A_Caro_ Reor.js
+++ 510535Nego_A_Caro_ Reor.js
@@ -468,11 +468,11 @@ function Mineria(browser,  Pin) {
             ComparacionCeldas = DetallesCompletos.ComparacionCeldas;
 
             const continCeldas = await page.$x('//span[contains(.,"Continuar")]');
-            await page.waitForTimeout(1500);
+            await page.waitForTimeout(1000);
             await continCeldas[1].click();
             
             console.log(IdArea);
-            await page.waitForTimeout(3000);
+            await page.waitForTimeout(900);
 
             const Todoslosparametros = await page.$$eval("span", links =>
                 links.map(link => link.textContent)
