7d99d92230f6b1be09ac1ca4a3129f1b80df1fea
CeereIngeniero1 <soporte2ceere@gmail.com>
2025-06-19 12:15:34 -0500

    list todo

--- 510535Nego_B_FER.js
+++ 510535Nego_B_FER.js
@@ -42,7 +42,7 @@ async function Pagina() {
         }
     });
     for (let i = 0; i < Pines.length; i++) {
-        if (Pines.substring(i + 1, i + 4) == 'N3:') {
+        if (Pines.substring(i + 1, i + 4) == 'N1:') {
             console.log(Pines.substring(i + 1, i + 4));
             Pin = Pines.substring(i + 4, i + 31);
             break
